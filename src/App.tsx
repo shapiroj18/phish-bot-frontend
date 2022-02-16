4360101d8c24e0eeb8912026eb1b0a22263b912a
Jonathan Shapiro <shapiroj18@gmail.com>
2022-02-16 14:50:58 -0500

    basic component addition and testing fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Player from './components/Player'
 
 function App() {
   return (
     <div className="Phishjams-main">
       <header className="App-header">
         <h1 className="Phishjams-title">Phishjams Garden</h1>
+        <Player />
       </header>
     </div>
   );
---
+++ src/components/Player.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Player.css';
+
+interface PlayerProps {
+    // musicInfo: any[];
+}
+
+class Player extends React.Component<PlayerProps> {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         data: null
+    //     };
+    // }
+
+    componentDidMount() {
+        fetch("http://phishjams-dev.herokuapp.com/get_song_info")
+        .then(response => response.json())
+        .then((object) => {
+            console.log(object)
+            this.setState({
+                data: object
+            });
+        })
+    }
+
+    render() {
+        // const { data } = this.props;
+      return <p>test_text</p>;
+    }
+  }
+
+export default Player;
